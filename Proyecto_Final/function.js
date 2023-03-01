1cc33b6c3423dc80743e65212780b9d8eee423b0
Hernan L <oportunidadbosques2017@gmail.com>
2023-03-01 14:22:48 -0300

    commit

---
+++ Proyecto_Final/function.js
@@ -0,0 +1,35 @@
+
+
+
+const titulo = document.querySelector('.titulo');
+console.log(contenedor)
+
+const contenido = document.querySelector('.contenido');
+console.log(contenido)
+
+const foto = document.querySelector('.foto');
+console.log(foto)
+
+const back = document.querySelector('.back');
+console.log(back)
+
+const misdatos = document.querySelector('.foto');
+console.log(misdatos)
+
+const controls = document.querySelector('controls')
+console.log(controls)
+
+const botons = document.querySelector('.botons')
+console.log(botons)
+
+window.addEventListener('click', e=> {console.log(e.target)})
+
+const imagenes = document.querySelectorAll('.img')
+
+const aside = document.querySelector('.aside')
+console.log(aside)
+
+
+
+
+   
